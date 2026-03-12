c92a7f5bc6cc168897d64f43767e666363745357
Sebastian Garcia <sebastian.garcia@risewithaurora.com>
2026-03-12 17:34:19 -0300

    initial commit

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,52 @@
+import { HeadContent, Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Toaster } from "@test-agents-behavior/ui/components/sonner";
+
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+
+import "../index.css";
+
+export interface RouterAppContext {}
+
+export const Route = createRootRouteWithContext<RouterAppContext>()({
+  component: RootComponent,
+  head: () => ({
+    meta: [
+      {
+        title: "test-agents-behavior",
+      },
+      {
+        name: "description",
+        content: "test-agents-behavior is a web application",
+      },
+    ],
+    links: [
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+    ],
+  }),
+});
+
+function RootComponent() {
+  return (
+    <>
+      <HeadContent />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+        storageKey="vite-ui-theme"
+      >
+        <div className="grid grid-rows-[auto_1fr] h-svh">
+          <Header />
+          <Outlet />
+        </div>
+        <Toaster richColors />
+      </ThemeProvider>
+      <TanStackRouterDevtools position="bottom-left" />
+    </>
+  );
+}
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { db } from "@test-agents-behavior/db";
+import * as schema from "@test-agents-behavior/db/schema/auth";
+import { env } from "@test-agents-behavior/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
